e97419530206aaa8d3415518edec52f94faf75e3
Markus <markusmattsson90@gmail.com>
2018-04-30 13:53:09 +0200

    Update login component

--- src/components/login.js
+++ src/components/login.js
@@ -5,6 +5,7 @@ class Login extends Component {
     
     state = {
         name: '',
+		loggedIn: false,
         signButton: 'Sign-In'
     }
     
@@ -15,17 +16,24 @@ class Login extends Component {
     }
     changeButton = () => {
         if(this.state.signButton === 'Sign-In') {
-            this.setState({signButton: 'Sign-Out'})
+            this.setState({
+				signButton: 'Sign-Out',
+				loggedIn: true
+			})
         } else {   
-            this.setState({signButton: 'Sign-In'})
+            this.setState({
+				signButton: 'Sign-In',
+				loggedIn: false,
+				name: ''
+			})
         }
     }
     render() {
     return (
         <div className="componentcontainer">
-            <input type="text" onChange={this.handleChange}/><br/>
+            <input type="text" onChange={this.handleChange} value={this.state.name}/><br/>
             <button onClick={this.changeButton}>{this.state.signButton}</button>
-
+			{this.state.loggedIn && <p>Welcome, {this.state.name}!</p>}
         </div>
     )
 }   
