c5a2f80fe8988d9aba0f84226cf2b3a462d9d82e
Markus <markusmattsson90@gmail.com>
2018-04-30 07:02:31 +0200

    Update numbergame

--- src/components/numbergame.js
+++ src/components/numbergame.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 class NumberGame extends Component {
 
+componentDidMount() {
+	this.setState({
+		winningNum: this.getRandomNum(1, 100)
+	});
+}
+	
 // Game values
     state = {
-        min: 1,
-        max: 100,
         winningNum: 55,
-        guessesLeft: 3,
         guessInput: 0,
+		numOfGuesses: 0,
         message: '',
         btn: 'Submit'
     }
@@ -19,48 +23,48 @@ class NumberGame extends Component {
 		})
 	}
 	
-	// Listen guess event
-    guessHandler = () => {
-    
-    	const guess = this.state.guessInput;
-    
-    
-    	// validate input
-    	if(isNaN(guess) || guess < this.state.min || guess > this.state.max){
-        this.setMessage(`Please enter a number between ${this.state.min} and ${this.state.max}`, 'red')
-		}
-
-		// check if won
-		if(guess === this.state.winningNum){
-			// disable input
-			this.gameOver(`${this.state.winningNum} is correct!, YOU WIN`);
-		} else {
-			// wrong number
+	btnHandler = () => {
+		if (this.state.btn === "Play Again") {
+			this.setMessage("");
 			this.setState({
-				guessesLeft: this.state.guessesLeft - 1
-			})
-
-			if(this.state.guessesLeft === 0){
-				this.gameOver(false,`Game over, you lost. The correct number was ${this.state.winningNum}`);
-
+				winningNum: this.getRandomNum(1, 100),
+				btn: "Submit",
+				numOfGuesses: 0
+			});
+		} else {
+			this.guessHandler();
+		}	
+	}
+	
+	incrementGuesses = () => {
+		this.setState({
+			numOfGuesses: this.state.numOfGuesses + 1
+		});
+	}
+	
+	// Listen guess event
+    guessHandler = () => { 
+		const guess = this.state.guessInput;
+    
+    	// check if input is valid
+    	if(isNaN(guess) || guess < 1 || guess > 100){
+        	this.setMessage("Please enter a number between 1 and 100")
+		//check if number is correct
+		} else if (guess === this.state.winningNum) {
+			// user wins
+			this.incrementGuesses();
+			this.gameOver(`${this.state.winningNum} is correct!, YOU WON with ${this.state.numOfGuesses} guesses!`);
+		//answer is wrong
+		} else {
+			this.incrementGuesses();
+			//if guess is higher
+			if (guess > this.state.winningNum) {
+				this.setMessage("The right answer is lower than your guess")
 			} else {
-				console.log(typeof(this.state.guessInput))
-				console.log(this.state.winningNum)
-				console.log(this.state.guessInput)
-				// game continues - answer wrong
-				// change border color
-				//guessInput.style.borderColor = 'red';
-				// clear input
-				this.setState({
-					guessInput: null
-				})
-				// tell user the wrong number
-				this.setMessage(`${guess} is not correct, ${this.state.guessesLeft} guesses left`, 'red');
+				this.setMessage("The right answer is higher than your guess")
 			}
 		}
-
 	};
-
 
 	// Game over
 	gameOver = (msg) => {
@@ -76,8 +80,8 @@ class NumberGame extends Component {
     	return Math.floor(Math.random()*(max-min+1)+min);
 	}
 
-    setMessage = (msg, color) => {
-        console.log(msg + color);
+    setMessage = (msg) => {
+        console.log(msg);
         this.setState({
             message: msg
         });
@@ -87,10 +91,16 @@ class NumberGame extends Component {
 			<div className="componentcontainer">
 				<h1>Number Game</h1>
 				<div id="game">
-			  		<p>Guess a number between <span className="min-num">{this.state.min}</span> and <span className="max-num">		{this.state.max}</span></p>
-				<input type="number" id="guess-input" placeholder="Enter your guess..." onChange={this.guessInputHandler}/>
-				<button id="guess-btn" onClick={this.guessHandler}>{this.state.btn}</button>
-				<p className="message">{this.state.message}</p>
+			  		<p>Guess a number between 1 and 100</p>
+					<input type="number" 
+						id="guess-input" 
+						placeholder="Enter your guess..."
+						onChange={this.guessInputHandler}/>
+					<button 
+						id="guess-btn" 
+						onClick={this.btnHandler}>{this.state.btn}
+					</button>
+					<p className="message">{this.state.message}</p>
 				</div>
 			 </div>
 		)
